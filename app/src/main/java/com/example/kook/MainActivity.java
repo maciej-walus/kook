aab67da663fe446d3d36a7d6e015e5fbb5f54e73
Jakub Zieliński <jakub.zieli02@gmail.com>
2023-06-13 16:38:49 +0200

    did

--- app/src/main/java/com/example/kook/MainActivity.java
+++ app/src/main/java/com/example/kook/MainActivity.java
@@ -3,10 +3,20 @@ package com.example.kook;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +24,68 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String directoryPath = "/data/data/com.example.kook/files/recepies"; // Specify the path to the directory you want to read
+
+        File directory = new File(directoryPath);
+        File[] directories = directory.listFiles(File::isDirectory);
+
+
+        if (directories != null) {
+            for (File dir : directories) {
+
+                //Creates the recepie widget as many times as there are directories
+                ConstraintLayout activity_main = findViewById(R.id.mainConstraintLayout);
+                LinearLayout categoryLinearLayout = findViewById(R.id.categoryLinearLayout);
+                LayoutInflater inflater = LayoutInflater.from(this);
+                View inflatedLayout = inflater.inflate(R.layout.widget, categoryLinearLayout, false);
+
+
+                //Set title
+                TextView titleTextView = inflatedLayout.findViewById(R.id.title);
+                titleTextView.setText(dir.getName());
+
+                //Set photo
+
+                ImageView imageView = inflatedLayout.findViewById(R.id.imageView2);
+
+                // Set the new image based on the file path
+                String filePath = "/data/data/com.example.kook/files/recepies/" + dir.getName() + "/photo.jpg";
+                File file = new File(filePath);
+                if (file.exists()) {
+                    Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+                    imageView.setImageBitmap(bitmap);
+                }
+                //Set button logic
+                Button yourButton = inflatedLayout.findViewById(R.id.kook_button);
+                yourButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Retrieve the modified title from the TextView
+                        TextView titleTextView = inflatedLayout.findViewById(R.id.title);
+                        String modifiedTitle = titleTextView.getText().toString();
+
+                        // Create an Intent to start the new activity
+                        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+
+                        // Add the modified title as an extra to the Intent
+                        intent.putExtra("Title", modifiedTitle);
+
+                        // Start the new activity
+                        startActivity(intent);
+                    }
+                });
+
+
+                categoryLinearLayout.addView(inflatedLayout);
+            }
+        }
+
     }
-    public void reload_recipies(View v){
-        ConstraintLayout activity_main = findViewById(R.id.mainConstraintLayout);
-        LinearLayout categoryLinearLayout = findViewById(R.id.categoryLinearLayout);
-        LayoutInflater inflater = LayoutInflater.from(this);
-        View inflatedLayout = inflater.inflate(R.layout.widget, categoryLinearLayout, false);
-        categoryLinearLayout.addView(inflatedLayout);
-    }
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/kook/MainActivity2.java
+++ app/src/main/java/com/example/kook/MainActivity2.java
@@ -2,7 +2,19 @@ package com.example.kook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,6 +22,109 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recipie);
+
+        Bundle b = getIntent().getExtras();
+        String parametr = b.getString("Title");
+        TextView poleTekstowe = (TextView) findViewById(R.id.Title);
+        poleTekstowe.setText(parametr);
+
+        update_recepie(parametr);
+
+
+    }
+    public static String replacePattern(String input, double doubleVariable) {
+        Pattern pattern = Pattern.compile("\\$(\\d+)(\\S+)?\\$");
+        Matcher matcher = pattern.matcher(input);
+        StringBuffer result = new StringBuffer();
+
+        while (matcher.find()) {
+            Double number = Double.parseDouble(matcher.group(1));
+            String replacement = number * doubleVariable +" " +matcher.group(2);
+            matcher.appendReplacement(result, replacement);
+        }
+        matcher.appendTail(result);
+
+        return result.toString();
+    }
+
+    public void update_recepie(String dir){
+        Context context = getApplicationContext();
+        File filePath = new File(context.getFilesDir(), "recepies/" + dir +"/recepie.txt");
+
+        List<String> ingredients = new ArrayList<>();
+        String text = "";
+        double portion_number = 0;
+
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+
+            String line;
+
+            boolean isIngredients = false;
+            boolean isText = false;
+            boolean isPortion = false;
+
+            while ( (line = br.readLine()) != null) {
+
+                if (line.contains("<ingredients>")) {
+                    isIngredients = true;
+                    continue;
+                }
+                if (line.contains("</ingredients>")) {
+                    isIngredients = false;
+                    continue;
+                }
+
+                if (line.contains("<portion>")){
+                    isPortion = true;
+                    continue;
+                }
+                if (line.contains("</portion>")){
+                    isPortion = false;
+                    continue;
+                }
+
+                if (line.contains("<text>")) {
+                    isText = true;
+                    continue;
+                }
+                if (line.contains("</text>")) {
+                    isText = false;
+                    continue;
+                }
+
+                if (isIngredients) {
+                    ingredients.add(replacePattern(line, portion_number));
+                } else if (isText) {
+
+                    text += replacePattern(line, portion_number);
+                }
+                else if (isPortion){
+                    portion_number = Double.parseDouble(line);
+                }
+
+
+
+
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String ingrid = "";
+
+        for(int i =0 ; i< ingredients.size() ; i++)
+        {
+            ingrid += ingredients.get(i)+"\n";
+        }
+
+        TextView modyfiko = (TextView) findViewById(R.id.ingredients);
+        modyfiko.setText(ingrid);
+
+        modyfiko = (TextView) findViewById(R.id.recipie);
+        modyfiko.setText(text);
+
     }
 
 }
